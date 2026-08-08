51a2cd62a7ddd4d4411234d82a97ae2053b37e92
kajaatharan <kajaatharan1112@gmail.com>
2026-08-08 11:02:02 +0530

    feat(work-management): add Add/RemoveObjectiveMember vertical slices

---
+++ src/ONEVO.Application/Features/WorkManagement/Objectives/Commands/RemoveObjectiveMember/RemoveObjectiveMemberCommand.cs
@@ -0,0 +1,6 @@
+using MediatR;
+using ONEVO.Application.Common.Models;
+
+namespace ONEVO.Application.Features.WorkManagement.Objectives.Commands.RemoveObjectiveMember;
+
+public sealed record RemoveObjectiveMemberCommand(Guid ObjectiveId, Guid UserId) : IRequest<Result>;
